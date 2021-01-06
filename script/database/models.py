adc5fcaaf7e1f25b2a94dfbae31b75800000ee6f
Bogdan <bmilanovic8818ri@raf.rs>
2021-01-06 01:13:07 +0100

    Database odradjen

---
+++ script/database/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+class Movie(models.Model):
+    content = models.CharField(max_length=30)
+    num_views = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Comment(models.Model):
+    content = models.TextField(max_length=400)
+    num_views = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
